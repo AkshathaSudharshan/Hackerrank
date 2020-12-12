5f1e79910084f93ea4480c6736ba4d8a37a086ab
AkshathaSudharshan <akshatha.s2496@gmail.com>
2020-12-12 15:36:31 +0530

    Add files via upload

---
+++ javasort.java
@@ -0,0 +1,75 @@
+import java.util.*;
+
+class Student{
+	private int id;
+	private String fname;
+	private double cgpa;
+	public Student(int id, String fname, double cgpa) {
+		super();
+		this.id = id;
+		this.fname = fname;
+		this.cgpa = cgpa;
+	}
+	public int getId() {
+		return id;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public double getCgpa() {
+		return cgpa;
+	}
+}
+
+//Complete the code
+class Sort implements Comparator<Student>
+{
+    public int compare(Student s1,Student s2)
+    {
+        int cgpacompare = Double.compare(s2.getCgpa(), s1.getCgpa());
+        int namecompare = s1.getFname().compareTo(s2.getFname());
+        int idcompare = s2.getId()-s1.getId();
+        if(cgpacompare==0)
+        {
+            if (namecompare==0)
+            {
+                return idcompare;
+            }
+            else
+            {
+                return namecompare;
+            }
+               
+        }
+
+        else
+            return cgpacompare;
+    }
+
+}
+public class Solution
+{
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+		
+		List<Student> studentList = new ArrayList<Student>();
+		while(testCases>0){
+			int id = in.nextInt();
+			String fname = in.next();
+			double cgpa = in.nextDouble();
+			
+			Student st = new Student(id, fname, cgpa);
+			studentList.add(st);
+			
+			testCases--;
+		}
+        Collections.sort(studentList,new Sort());
+      	for(Student st: studentList){
+			System.out.println(st.getFname());
+		}
+	}
+}
+
+
+
